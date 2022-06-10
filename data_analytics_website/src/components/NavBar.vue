2440804a303a99b8598c8c4c0166afe983d8e93d
rory-wagner <red42king@gmail.com>
2022-06-09 23:39:54 -0600

    Added in NavBar

---
+++ data_analytics_website/src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div id="top-bar">
+      <img>
+      <h2>
+        Website Title
+      </h2>
+    </div>
+    <div id="links-bar">
+      <a class="links" style="flex-grow: 3">About</a>
+      <a class="links">Portfolio</a>
+      <a class="links" style="flex-grow: 3">Contact</a>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style>
+
+</style>
+
+<style scoped>
+  #top-bar{
+    width: 100%;
+    text-align: center;
+  }
+
+  #links-bar{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+  }
+
+  .links{
+    width: 100%;
+    text-align: center;
+  }
+</style>
